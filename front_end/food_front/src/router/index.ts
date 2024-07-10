724c497034053aacf8e03f5d7132e70b857a9b4d
YoungHappy <childking0909@gmail.com>
2024-07-10 11:31:42 +0800

    food-pot

--- front_end/food_front/src/router/index.ts
+++ front_end/food_front/src/router/index.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-
+import FoodPotPage from '../views/FoodPotPage.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -16,6 +16,11 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/food-pot',
+      name: 'FoodPotPage',
+      component: FoodPotPage
     }
   ]
 })
---
+++ front_end/food_front/src/views/FoodPotPage.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="food-pot-page">
+      <div class="container">
+        <FoodList @food-selected="handleFoodSelected" />
+        <Pot :selectedFood="selectedFood" />
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import FoodList from '@/components/FoodList.vue';
+  import Pot from '@/components/Pot.vue';
+  
+  export default {
+    components: {
+      FoodList,
+      Pot,
+    },
+    data() {
+      return {
+        selectedFood: null,
+      };
+    },
+    methods: {
+      handleFoodSelected(food) {
+        this.selectedFood = food;
+      },
+    },
+  };
+  </script>
+  
+  <style>
+  .container {
+    display: flex;
+    justify-content: space-between;
+  }
+  @media (max-width: 600px) {
+    .container {
+      flex-direction: column;
+      align-items: center;
+    }
+  }
+  </style>
+  
