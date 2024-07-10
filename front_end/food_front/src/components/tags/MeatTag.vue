9f2c5593b091145695b7bc833de0c00bdd345ffe
YoungHappy <childking0909@gmail.com>
2024-07-10 15:25:42 +0800

    foodselect

---
+++ front_end/food_front/src/components/tags/MeatTag.vue
@@ -0,0 +1,28 @@
+<template>
+    <span
+      class="meat-tag rounded tag"
+      p="x-2"
+      border="~ red-200 dark:red-800"
+      :class="[
+        active ? 'bg-red-600 opacity-90' : 'bg-red-300 opacity-20',
+        active ? 'text-red-100' : 'text-red-800 dark:text-red-200'
+      ]"
+    >
+      <slot />
+    </span>
+  </template>
+  
+  <script lang="ts" setup>
+  const props = defineProps<{
+    active: boolean
+  }>()
+  </script>
+  
+  <style scoped>
+  .meat-tag {
+    padding: 0.5rem 1rem;
+    border: 1px solid;
+    border-radius: 0.25rem;
+  }
+  </style>
+  
---
+++ front_end/food_front/src/components/tags/VegetableTag.vue
@@ -0,0 +1,30 @@
+<template>
+    <span
+      class="vegetable-tag rounded tag"
+      p="x-2"
+      border="~ green-200 dark:green-800"
+      :class="[
+        active ? 'bg-green-600 opacity-90' : 'bg-green-300 opacity-20',
+        active ? 'text-green-100' : 'text-green-800 dark:text-green-200'
+      ]"
+    >
+      <slot />
+    </span>
+  </template>
+  
+  
+  <script lang="ts" setup>
+  // 赋予属性，是否选中
+  const props = defineProps<{
+    active: boolean
+  }>()
+  </script>
+  
+  <style scoped>
+  .vegetable-tag {
+    padding: 0.5rem 1rem;
+    border: 1px solid;
+    border-radius: 0.25rem;
+  }
+  </style>
+  
