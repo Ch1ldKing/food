a86b9aee00d8e05c21433067ae0a9ffd91f98816
JKFlipFlop1108 <GAOYU1108S@GMAIL.COM>
2024-07-18 17:48:13 +0800

    按下后的阴影

--- front_end/food_front/src/views/linked.vue
+++ front_end/food_front/src/views/linked.vue
@@ -142,7 +142,8 @@ const handleClick = (index) => {
   justify-content: center;
   color: #039be5; /* 设置文字颜色为蓝色 */
   font-family: "Century Gothic", CenturyGothic, AppleGothic, sans-serif;
-  font-weight: bold; /* 设置文字加粗 */
+  font-weight: 700; /* 设置文字加粗 */
+
 }
 
 
@@ -156,6 +157,14 @@ const handleClick = (index) => {
 /* 激活状态的气泡样式 */
 .bubble.active {
   transform: scale(1.1);
+
+  box-shadow: inset 4px 4px 6px -1px rgba(0,0,0,0.2),
+	      inset -4px -4px 6px -1px rgba(255,255,255,0.7),
+	      -0.5px -0.5px 0px rgba(255,255,255,1),
+	      0.5px 0.5px 0px rgba(0,0,0,0.15),
+	      0px 12px 10px -10px rgba(0,0,0,0.05);
+  border: 1px solid rgba(0,0,0,0.1);
+  transform: translateY(0.5em);
 }
 </style>
 
